e706a446f0bf33a50389ec61c44fbf15a2e2e714
Claude Jiang <jwz426@gmail.com>
2023-02-11 13:27:34 +0800

    support pwa

--- build/build.go
+++ build/build.go
@@ -14,5 +14,8 @@ func main() {
 	builder.TaskForEditorAssets("embed/assets/vendor/editor-assets", "internal/editor/editor-assets")
 	builder.TaskForStyles("internal/state/style.go")
 	builder.TaskForFavicon("embed/assets/favicon.ico", "internal/resources/assets/favicon.ico")
+	builder.TaskForFavicon("embed/assets/icons/favicon/android-chrome-192x192.png", "internal/resources/assets/android-chrome-192x192.png")
+	builder.TaskForFavicon("embed/assets/icons/favicon/android-chrome-512x512.png", "internal/resources/assets/android-chrome-512x512.png")
+	builder.TaskForFavicon("embed/assets/icons/favicon/manifest.json", "internal/resources/assets/manifest.json")
 	builder.TaskForTemplates("embed/templates", "internal/resources/templates/html")
 }
--- internal/resources/assets/assets.go
+++ internal/resources/assets/assets.go
@@ -16,6 +16,15 @@ import (
 //go:embed favicon.ico
 var Favicon embed.FS
 
+//go:embed android-chrome-192x192.png
+var Png192 embed.FS
+
+//go:embed android-chrome-512x512.png
+var Png512 embed.FS
+
+//go:embed manifest.json
+var Manifest embed.FS
+
 func RegisterRouting(router *gin.Engine) {
 
 	router.Use(optimizeResourceCacheTime())
@@ -23,6 +32,21 @@ func RegisterRouting(router *gin.Engine) {
 	router.GET("/favicon.ico", func(c *gin.Context) {
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.FileFromFS("favicon.ico", http.FS(Favicon))
+	})
+
+	router.GET("/android-chrome-192x192.png", func(c *gin.Context) {
+		c.Header("Cache-Control", "public, max-age=31536000")
+		c.FileFromFS("android-chrome-192x192.png", http.FS(Png192))
+	})
+
+	router.GET("/android-chrome-512x512.png", func(c *gin.Context) {
+		c.Header("Cache-Control", "public, max-age=31536000")
+		c.FileFromFS("android-chrome-512x512.png", http.FS(Png512))
+	})
+
+	router.GET("/manifest.json", func(c *gin.Context) {
+		c.Header("Cache-Control", "public, max-age=31536000")
+		c.FileFromFS("manifest.json", http.FS(Manifest))
 	})
 
 	if FlareState.AppFlags.DebugMode {
